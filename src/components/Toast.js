68e7f5d67a3777a9ed5a8c209b653bcd84aea88c
suk924600 <suk924600@naver.com>
2024-11-25 11:26:57 +0900

    전체 코드로직 변경

--- src/components/Toast.js
+++
@@ -1,8 +0,0 @@
-const Toast = ({ message }) => (
-    <div className="fixed bottom-4 right-4 bg-gray-800 text-white py-2 px-4 rounded shadow-lg">
-      {message}
-    </div>
-  );
-  
-  export default Toast;
-  
